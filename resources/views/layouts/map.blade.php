10d53dfdb2dd1732cd44200f56a1f12e5bb8b624
unknown <glenbenatiro@gmail.com>
2019-11-03 15:28:22 +0800

    changed stylesheets linking to fix css issue in heroku

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 
     <title>Bike Renting Service: Byke Philippines</title>
 
