82d9419cc971ab01086f59801e63223108510354
unknown <glenbenatiro@gmail.com>
2019-11-03 16:54:36 +0800

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
       <div class="flex w-1/2 flex-col">
           <p class="text-splash font-bold text-green-600">Bikes.</p>
           <p class="text-splash font-thin">Great for you.<br /> Great for the environment.</p>
-          <button class="mt-6 w-splash text-6xl bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-6 rounded-full">
+          <a href="/rent" class="mt-6 w-splash text-center text-6xl bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-6 rounded-full">
             Rent a Bike
-          </button>
+          </a>
       </div>
 
     <div class="flex flex-col w-1/2 items-center">
