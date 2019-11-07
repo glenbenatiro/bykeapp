b4592b7b2829e109bd48ea85e8e54fe5f7e84336
glenbenatiro <glenbenatiro@gmail.com>
2019-11-07 18:38:10 +0800

    paymaya wip

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form method="get" action="https://pg-sandbox.paymaya.com/payments/v1/customers/198cae1c-b35c-45bd-8acc-7a4d48372069">"
+
+</form>
+
+@endsection
