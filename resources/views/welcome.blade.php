a40824009113ca7f327e54ed601b1788cd254244
Honey Marie Benatiro <glenbenatiro@gmail.com>
2019-11-09 15:48:55 +0800

    final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
 
 @section('content')
 <div class="mt-12 flex flex-col items-center">
-    <p class="text-splash font-thin text-center"><span class="font-bold text-green-600">Bikes.</span> Great for you.</p>
-    <p class="text-splash font-thin text-center">Great for the environment.</p>
+    <p class="text-splash font-thin text-white text-center"><span class="font-bold text-green-300">Bikes.</span> Great for you.</p>
+    <p class="text-splash font-thin text-white text-center">Great for the environment.</p>
     <a href="/rent"
         class="mt-6 w-1/2 text-center text-xl bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-6 rounded-full">
         Rent a Bike
